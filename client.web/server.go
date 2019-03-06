b6ce7275759e135370c75c3ca6acb41fbca884ef
Ruben <rwoldhui@avans.nl>
2019-03-06 21:49:16 +0100

    [Removed] deprecated modules

--- client.web/server.go
+++ client.web/server.go
@@ -9,6 +9,11 @@ import (
 func main() {
 	fs := http.FileServer(http.Dir("public"))
 	http.Handle("/", fs)
+	/* Testing the service.config from another image
+	resp, err := http.Get("http://service.config/config/1")
+	fmt.Println(resp.Status)
+	fmt.Println(err)
+	*/
 	fmt.Println("Starting front-end server on port 80...")
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
--- service.config/server.go
+++ service.config/server.go
@@ -21,9 +21,9 @@ func main() {
 	loadConfigs()
 	router := mux.NewRouter()
 
-	router.HandleFunc("/config/update/{id}", updateConfigEndpoint).Methods("POST")
-	router.HandleFunc("/config/delete/{id}", deleteConfigEndpoint).Methods("DELETE")
-	router.HandleFunc("/config/get/{id}", getConfigEndpoint).Methods("GET")
+	router.HandleFunc("/config/{id}", updateConfigEndpoint).Methods("POST")
+	router.HandleFunc("/config/{id}", deleteConfigEndpoint).Methods("DELETE")
+	router.HandleFunc("/config/{id}", getConfigEndpoint).Methods("GET")
 
 	fmt.Println("Starting IMC server on port 80...")
 	log.Fatal(http.ListenAndServe(":80", router))
@@ -41,20 +41,48 @@ func loadConfigs() {
 	fmt.Printf("Loaded %d configs.\n", len(configs.Configs))
 }
 
-func getConfigEndpoint(w http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	id := vars["id"]
-	fmt.Printf("Got request for config with ID: %s.\n", id)
+// Enables CORS for http responses
+func enableCORS(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
-func deleteConfigEndpoint(w http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	id := vars["id"]
-	fmt.Printf("Got request for config with ID: %s.\n", id)
-}
-
+// POST: Updates the config of 1 radio. Has a radioID as var. Receives JSON Config object as body.
 func updateConfigEndpoint(w http.ResponseWriter, req *http.Request) {
+	// Get radioID from vars
 	vars := mux.Vars(req)
-	id := vars["id"]
-	fmt.Printf("Got request for config with ID: %s.\n", id)
+	fmt.Println("Radio ID:", vars["radioID"])
+
+	// Decode Config from body
+	var c Config
+	if err := json.NewDecoder(req.Body).Decode(&c); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(c)
+	// Begin Response
+	enableCORS(&w)
+	w.WriteHeader(200)
+}
+
+// GET: Get the config of 1 radio. Has a radioID as var.
+func getConfigEndpoint(w http.ResponseWriter, req *http.Request) {
+	// Get radioID from vars
+	vars := mux.Vars(req)
+	fmt.Println("ID:", vars["id"])
+
+	// Begin Response
+	enableCORS(&w)
+	w.WriteHeader(200)
+}
+
+// GET: Get the config of 1 radio. Has a radioID as var.
+func deleteConfigEndpoint(w http.ResponseWriter, req *http.Request) {
+	// Get radioID from vars
+	vars := mux.Vars(req)
+	fmt.Println("ID:", vars["id"])
+
+	// Begin Response
+	enableCORS(&w)
+	w.WriteHeader(200)
 }
